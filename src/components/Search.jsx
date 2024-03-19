c89e97fcd5c4a9150ef47a619defb15b00fedd7b
cero-dev <cero-dev@users.noreply.github.com>
2024-03-18 23:08:25 -0500

    Initial file structure, and add searchbar with button to page

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <>
+    <form action="">
+        <input type="text" placeholder="Enter A Marvel Character"/>
+        <button type="submit">Search</button>
+    </form>
+    </>
+  )
+}
